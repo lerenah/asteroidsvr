a4427e124951fe98e7526826a2760404c77493a6
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-02 23:25:06 -0400

    removed files and backgrounds

--- WelcomeToVR/index.vr.js
+++ WelcomeToVR/index.vr.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import { AppRegistry, asset, Pano, Text, View } from 'react-vr';
 
 export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('stars.jpg')}/>
+        <Pano source={asset('stars.jpg')} />
         <Text
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
+            fontSize: 0.5,
+            fontWeight: '200',
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
+            transform: [{ translate: [0, 0, -3] }]
+          }}
+        >
+          Asteroids of NASA
         </Text>
       </View>
     );
   }
-};
+}
 
 AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
